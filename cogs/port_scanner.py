965afabc1b9202a1e67a58020d73902d24ab1183
Shay <shalinsingh818@gmail.com>
2021-12-16 11:33:28 -0500

    Added port scanner

--- cogs/port_scanner.py
+++ cogs/port_scanner.py
@@ -15,24 +15,47 @@ class PortScanner(commands.Cog):
 		print("Port scanning service is online... ")
 
 	
+	def scanHost(self, ip, startPort, endPort):
+		""" Starts a TCP scan on a given IP address """
+
+		# Begin TCP scan on host
+		results = self.tcp_scan(ip, startPort, endPort)
+		return results
+
+	
+	def scanRange(self, network, startPort, endPort):
+		""" Starts a TCP scan on a given IP address range """
+
+		# Iterate over a range of host IP addresses and scan each target
+		for host in range(1, 255):
+			ip = network + '.' + str(host)
+			self.tcp_scan(ip, startPort, endPort)
+
+
+	def tcp_scan(self, ip, startPort, endPort):
+		""" Creates a TCP socket and attempts to connect via supplied ports """
+		msg = "**PORT SCANNER RESULTS** for *{}* \n\n".format(ip)
+		for port in range(startPort, endPort + 1):
+			try:
+				# Create a new socket
+				tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+				# Print if the port is open
+				if not tcp.connect_ex((ip, port)):
+					value = '*OPEN*:  __{}/TCP__ \n'.format(port)
+					msg += value
+					tcp.close()
+				
+			except Exception:
+				pass
+
+		return msg
+	
+	
 	@commands.command()
-	async def start_scan(self,ctx, hostname: str):
-		target = socket.gethostbyname(hostname)
-		print("HOSTNAME: {} ".format(hostname))
-		try:
-			print("starting scan ")
-			for port in range(1, 65535):
-				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				con = s.connect((target, port))
-				if con:
-					print("{} is open ".format(port))
-				s.close()
-
-		except socket.gaierror:
-			print("\n Hostname Could Not Be Resolved !!!!")
-		except socket.error:
-			print("\ Server not responding !!!!")
-
+	async def port_scan(self,ctx, hostname: str, startPort, endPort):
+		socket.setdefaulttimeout(0.01)
+		results = self.scanHost(hostname, int(startPort), int(endPort))
+		await ctx.send(results)
 
 def setup(client):
 	client.add_cog(PortScanner(client))
